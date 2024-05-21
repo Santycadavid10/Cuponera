c5bbbc937c5df536c150aa44699072adf95bb1f4
santy <santiagocadavidrotavista@gmail.com>
2024-05-21 16:58:22 -0500

    ANtyyy

--- wwwroot/js/CrearCupones.js
+++ wwwroot/js/CrearCupones.js
@@ -10,33 +10,36 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const selectedTiendasElement = document.getElementById("selectedTiendas");
   const hiddenInput = document.getElementById("TiendasCupon");
   let selectedTiendas = [];
+  let selectedTiendasId = [];
 
   checkboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", (event) => {
       const checkbox = event.target;
       const tiendaNombre = checkbox.value;
+      const tiendaId = checkbox.id;
 
       if (checkbox.checked) {
         selectedTiendas.push(tiendaNombre);
+        selectedTiendasId.push(tiendaId);
       } else {
         selectedTiendas = selectedTiendas.filter((nombre) => nombre !== tiendaNombre);
+        selectedTiendasId = selectedTiendasId.filter((id) => id !== tiendaId);
       }
 
       selectedTiendasElement.textContent = selectedTiendas.join(", ");
-      TiendasCupon.value = selectedTiendas.join(", ");
+      hiddenInput.value = selectedTiendasId.join(", ");
     });
   });
 });
 
-
 function mostrarImagen(input) {
   var reader = new FileReader();
-  
-  reader.onload = function(e) {
-      var imagenMostrada = document.getElementById('imagenMostrada');
-      imagenMostrada.src = e.target.result;
-      imagenMostrada.style.display = 'block'; // Mostrar la imagen
+
+  reader.onload = function (e) {
+    var imagenMostrada = document.getElementById("imagenMostrada");
+    imagenMostrada.src = e.target.result;
+    imagenMostrada.style.display = "block"; // Mostrar la imagen
   };
-  
+
   reader.readAsDataURL(input.files[0]); // Leer el archivo como una URL
 }
